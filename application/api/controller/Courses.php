7fd7c908876620000bf12c5dd9e822fe7a8febb4
SevenMon <57867782@qq.com>
2019-09-20 16:43:57 +0800

    'sdf'

--- application/api/controller/Courses.php
+++ application/api/controller/Courses.php
@@ -1,50 +1,15 @@
 <?php
 namespace app\api\controller;
 
-use think\Controller;
 use think\Db;
 use think\Request;
 
-class Courses extends Controller
+class Courses extends Base
 {
-    public $edu_user_info = '';
-    public $user_info = '';
-    public $campus_arr = '';
-
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
         $this->checkLogin();
-    }
-
-    public function checkLogin(){
-        $token = input('token');
-        $info = Db::name('admin')->where('token','=',$token)->find();
-        if($info == null){
-            echo json_encode(array(
-                'status' => 101,
-                'msg' => '登录超时，请重新登录！',
-                'data' => array(
-                )
-            ));
-            exit();
-        }
-        $this->user_info = $info;
-
-        $edu_db = Db::connect(config('edu_database'));
-        $edu_admin_info = $edu_db->name('admin')->where('uid','=',$info['edu_uid'])->find();
-        if($edu_admin_info == null){
-            echo json_encode(array(
-                'status' => 101,
-                'msg' => '用户不存在请重新登陆',
-                'data' => array()
-            ));
-            exit();
-        }
-        $this->edu_user_info = $edu_admin_info;
-        $where = array();
-        $where['admin_id'] = $edu_admin_info['uid'];
-        $this->campus_arr = $edu_db->name('admin_campus')->where($where)->column('campus_id');
     }
 
     public function index(){
