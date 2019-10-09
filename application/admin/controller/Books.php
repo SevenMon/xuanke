e2941fd8c0457d9f412cc7765498e3d94bffcd24
SevenMon <57867782@qq.com>
2019-10-09 17:06:55 +0800

    'sdf'

--- application/admin/controller/Books.php
+++ application/admin/controller/Books.php
@@ -57,7 +57,7 @@ class Books extends Base
         if(!empty($cat_id2)){
             $course_where['cat_id3'] = $cat_id3;
         }
-        $condition['course_id'] = array('in',$this->campus_arr);
+        $course_where['campus_id'] = array('in',$this->campus_arr);
         if(!empty($course_where)){
             $course_id_arr = Db::name('course')->where($course_where)->column('id');
             $condition['course_id'] = array('in',array_merge($course_id_arr,$this->campus_arr));
